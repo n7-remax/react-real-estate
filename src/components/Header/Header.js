30c98ffef3d783619fb299d04f6db17690762f2f
n7-remax <ditritusa@gmail.com>
2021-01-11 22:01:36 +0200

    added dotted button to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import logo from "../../assets/logo.svg";
-import headerButtons from "../../assets/header-button.svg";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="header">
       <div className="container">
@@ -10,15 +11,23 @@ const Header = () => {
             <img src={logo} alt="logo" />
           </div>
           <div className="lang-switch">
-            <span className="selected">
-              UA
-            </span>
+            <span className="selected">UA</span>
             <span>RU</span>
             <span>ENG</span>
           </div>
         </div>
         <div className="header-button">
-          <img src={headerButtons} alt="header-button" />
+          <a href="#" class={isOpen ? "menu active" : "menu"} onClick={()=> setIsOpen(!isOpen)}>
+            <div class="pix"></div>
+            <div class="pix"></div>
+            <div class="pix"></div>
+            <div class="pix"></div>
+            <div class="pix"></div>
+            <div class="pix"></div>
+            <div class="pix"></div>
+            <div class="pix"></div>
+            <div class="pix"></div>
+          </a>
         </div>
       </div>
     </header>
