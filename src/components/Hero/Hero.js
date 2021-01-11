765b9dd2b7acce95fb503689e46598bcc4a00bef
n7-remax <ditritusa@gmail.com>
2021-01-12 01:42:18 +0200

    finished half hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,24 @@
 const Hero = () => {
-    return(<div>Hero</div>)
-}
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="about-company">
+          <div className="button-default button-company">
+            <span>ПРО КОМПАНІЮ</span>
+          </div>
+          <div className="button-default arrow-right">
+            <span></span>
+          </div>
+        </div>
+        <div className="promotion">
+          <div className="promotion-info-box">
+            <h1>Надійний забудовник</h1>
+            <p>- це 7 років успішної роботи на українському ринку. Понад 700 квартир вже продано, а в планах компанії побудувати 120 тис. Метрів житлової площі.</p>
+          </div>
+        </div>
+        <div className="slider"></div>
+      </div>
+    </div>
+  );
+};
 export default Hero;
