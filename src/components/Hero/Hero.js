8c87822966cef60b9a9f72696bf4322ad58b41c2
n7-remax <ditritusa@gmail.com>
2021-01-12 18:09:10 +0200

    finished hero template and logic for slider

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,27 @@
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+  EffectFade,
+} from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import sliderImg from "../../assets/slider-img.png";
+
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+import { useState } from "react";
+
+// install Swiper components
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade]);
+
 const Hero = () => {
+  const [currentSlide, setCurrentSlider] = useState(0);
   return (
     <div className="hero">
       <div className="container">
@@ -13,10 +36,80 @@ const Hero = () => {
         <div className="promotion">
           <div className="promotion-info-box">
             <h1>Надійний забудовник</h1>
-            <p>- це 7 років успішної роботи на українському ринку. Понад 700 квартир вже продано, а в планах компанії побудувати 120 тис. Метрів житлової площі.</p>
+            <p>
+              - це 7 років успішної роботи на українському ринку. Понад 700
+              квартир вже продано, а в планах компанії побудувати 120 тис.
+              Метрів житлової площі.
+            </p>
           </div>
         </div>
-        <div className="slider"></div>
+        <div className="slider">
+          <div className="slider-nav">
+            <div
+              className={
+                currentSlide !== 0
+                  ? "swiper-button-prev-unique button-slanted active-slider-button"
+                  : "swiper-button-prev-unique button-slanted"
+              }
+            ></div>
+            <div
+              className={
+                currentSlide !== 3
+                  ? "swiper-button-next-unique button-slanted active-slider-button"
+                  : "swiper-button-next-unique button-slanted"
+              }
+            ></div>
+          </div>
+          <div className="slider-main">
+            <div className="slider-desc">
+              <div className="swiper-pagination"></div>
+              <h3>Акційна пропозиція №{currentSlide + 1}</h3>
+              <p>
+                Забудовник - це 5 років успішної роботи на українському ринку.
+                Успішної роботи на українському ринку.
+              </p>
+            </div>
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={1}
+              autoplay={true}
+              fadeEffect={true}
+              navigation={{
+                nextEl: ".swiper-button-next-unique",
+                prevEl: ".swiper-button-prev-unique",
+              }}
+              pagination={{
+                el: ".swiper-pagination",
+                clickable: true,
+                renderBullet: (index, className) => {
+                  return (
+                    '<div class="' +
+                    className +
+                    '">' +
+                    ("0" + (index + 1)) +
+                    "</div>"
+                  );
+                },
+              }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={(swiper) => setCurrentSlider(swiper.activeIndex)}
+            >
+              <SwiperSlide virtualIndex={1}>
+                <img src={sliderImg} alt="img" />
+              </SwiperSlide>
+              <SwiperSlide virtualIndex={2}>
+                <img src={sliderImg} alt="img" />
+              </SwiperSlide>
+              <SwiperSlide virtualIndex={3}>
+                <img src={sliderImg} alt="img" />
+              </SwiperSlide>
+              <SwiperSlide virtualIndex={4}>
+                <img src={sliderImg} alt="img" />
+              </SwiperSlide>
+              ...
+            </Swiper>
+          </div>
+        </div>
       </div>
     </div>
   );
