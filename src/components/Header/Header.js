b481b94a5d0a3e7a7b215d999397948305bd3d5d
n7-remax <ditritusa@gmail.com>
2021-01-13 16:57:13 +0200

    added dropdown menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.svg";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  // toggle scroll on header menu open/close
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isOpen]);
   return (
     <header className="header">
       <div className="container">
@@ -17,19 +26,42 @@ const Header = () => {
           </div>
         </div>
         <div className="header-button">
-          <a href="#" class={isOpen ? "menu active" : "menu"} onClick={()=> setIsOpen(!isOpen)}>
-            <div class="pix"></div>
-            <div class="pix"></div>
-            <div class="pix"></div>
-            <div class="pix"></div>
-            <div class="pix"></div>
-            <div class="pix"></div>
-            <div class="pix"></div>
-            <div class="pix"></div>
-            <div class="pix"></div>
+          <a
+            href="#"
+            className={isOpen ? "menu active" : "menu"}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <div className="pix"></div>
+            <div className="pix"></div>
+            <div className="pix"></div>
+            <div className="pix"></div>
+            <div className="pix"></div>
+            <div className="pix"></div>
+            <div className="pix"></div>
+            <div className="pix"></div>
+            <div className="pix"></div>
           </a>
         </div>
       </div>
+      {isOpen ? (
+        <div className="nav-menu">
+          <nav>
+            <a href="#">Головна</a>
+            <a href="#">Про компанію</a>
+            <a href="#">Варіанти придбання</a>
+            <a href="#">Варіанти інвестування</a>
+            <a href="#">Новини</a>
+            <a href="#">Документи</a>
+            <a href="#">Контакти</a>
+            <a href="#">Співробітництво</a>
+          </nav>
+          <div className="copyright">
+            <p>
+              Дизайн та розробка - <span>Svitsoft</span>
+            </p>
+          </div>
+        </div>
+      ) : null}
     </header>
   );
 };
